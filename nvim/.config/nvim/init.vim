f2aeb91e76fd514acb7b97992f2b9a4d40fcb06e
Guilherme Oliveira <htoguilherme@gmail.com>
2022-03-13 23:19:02 +1300

    feat: add nvim config

---
+++ nvim/.config/nvim/init.vim
@@ -0,0 +1,112 @@
+" ==== [SETS]
+
+set exrc
+set tabstop=4 softtabstop=4
+set shiftwidth=4
+set guicursor=
+set relativenumber
+set nohlsearch
+set hidden
+set noerrorbells
+set expandtab
+set smartindent
+set nu
+set nowrap
+set noswapfile
+set nobackup
+set undodir=~/.vim/undodir
+set undofile
+set incsearch
+set termguicolors
+set scrolloff=8
+set background=dark
+set completeopt=menu,menuone,noselect
+
+" ==== [PLUGINS]
+
+call plug#begin()
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'nvim-telescope/telescope-project.nvim'
+Plug 'matsuuu/pinkmare'
+Plug 'neovim/nvim-lspconfig'
+Plug 'tami5/lspsaga.nvim'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/vim-vsnip'
+Plug 'onsails/lspkind-nvim'
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+Plug 'dense-analysis/ale'
+Plug 'preservim/nerdtree'
+Plug 'dstein64/vim-startuptime'
+Plug 'tpope/vim-surround'
+Plug 'arcticicestudio/nord-vim'
+Plug 'nvim-lua/plenary.nvim' 
+Plug 'airblade/vim-rooter'
+Plug 'nvim-lualine/lualine.nvim'
+Plug 'kyazdani42/nvim-web-devicons'
+Plug 'ryanoasis/vim-devicons'
+call plug#end()
+
+" ==== [GENERAL]
+
+let mapleader = " "
+colorscheme nord
+set encoding=utf8
+let g:rooter_targets = '/,*'
+let g:rooter_patterns = ['.git']
+let g:ale_fixers = ['prettier', 'eslint']
+let g:ale_fix_on_save = 1
+
+" ==== [LUA]
+
+lua << EOF
+require('init')
+EOF
+
+" ==== [KEYBINDS]
+
+imap jk <Esc>
+
+nnoremap <Tab> <cmd>:bnext<CR>
+nnoremap <S-Tab> <cmd>:bprevious<CR>
+
+nnoremap <leader>ff <cmd>lua require'telescope.builtin'.find_files{}<CR>
+nnoremap <leader>fp <cmd>lua require'telescope.builtin'.git_files{}<CR>
+nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+nnoremap <leader>fb <cmd>Telescope buffers<cr>
+nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+nnoremap <leader>pp <cmd>:lua require'telescope'.extensions.project.project{}<cr>
+
+nnoremap <leader>ft <cmd>NERDTreeToggleVCS<cr>
+nnoremap <leader>fT <cmd>NERDTreeFind<cr>
+
+nnoremap <leader>wh <cmd>:split<cr>
+nnoremap <leader>wv <cmd>:vsplit<cr>
+nnoremap <leader>wd <cmd>:q<cr>
+nnoremap <leader>h <cmd>:wincmd h<cr>
+nnoremap <leader>j <cmd>:wincmd j<cr>
+nnoremap <leader>k <cmd>:wincmd k<cr>
+nnoremap <leader>l <cmd>:wincmd l<cr>
+
+nnoremap <leader>cd <cmd>:Lspsaga lsp_finder<CR>
+nnoremap <leader>cD <cmd>lua vim.lsp.buf.definition()<CR>
+nnoremap <leader>cr <cmd>:Lspsaga rename<CR>
+nnoremap <leader>csd <cmd>:Lspsaga hover_doc<CR>
+nnoremap <silent> <C-f> <cmd>:lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>
+nnoremap <silent> <C-b> <cmd>:lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>
+nnoremap <leader>csa <cmd>:Lspsaga code_action<CR>
+nnoremap <leader>css <cmd>:Lspsaga signature_help<CR>
+nnoremap <leader>cse <cmd>:Lspsaga show_line_diagnostics<CR>
+
+nnoremap <leader>tn <cmd>:tabnew<cr>
+nnoremap <leader>tc <cmd>:tabclose<cr>
+nnoremap <leader>t1 1gt
+nnoremap <leader>t2 2gt
+nnoremap <leader>t3 3gt
+nnoremap <leader>t4 4gt
+nnoremap <leader>t5 5gt
+
---
+++ nvim/.config/nvim/lua/init.lua
@@ -0,0 +1,125 @@
+require'lspconfig'.tsserver.setup{}
+require'lspconfig'.cssls.setup{}
+require'lspconfig'.emmet_ls.setup{}
+require'lualine'.setup{}
+require'nvim-treesitter.configs'.setup { highlight = { enable = true } }
+require'telescope'.load_extension('project')
+
+local lspconfig = require'lspconfig'
+local configs = require'lspconfig/configs'    
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.emmet_ls.setup({
+    -- on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "html", "css", "typescriptreact", "javascriptreact" },
+})
+
+ local cmp = require'cmp'
+ local lspkind = require('lspkind')
+
+  cmp.setup({
+    formatting = {
+       format = lspkind.cmp_format(),
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    }),
+     snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    mapping = {
+      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<C-Space>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    },
+
+    ['<Tab>'] = function(fallback)
+      if not cmp.select_next_item() then
+        if vim.bo.buftype ~= 'prompt' and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end
+    end,
+
+    ['<S-Tab>'] = function(fallback)
+      if not cmp.select_prev_item() then
+        if vim.bo.buftype ~= 'prompt' and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end
+    end,
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+ --      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it. 
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Setup lspconfig.
+  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  require('lspconfig')['tsserver'].setup {
+    capabilities = capabilities
+  }
+
+  require('lspconfig')['cssls'].setup {
+    capabilities = capabilities
+  }
