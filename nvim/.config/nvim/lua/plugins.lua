d82720c58a67b3b1be5468bdc39832126ea6f7b0
Guilherme Oliveira <htoguilherme@gmail.com>
2023-01-30 15:38:00 +1300

    feat: plugins cleanup and formatting

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -28,26 +28,141 @@ require("lazy").setup({
 		end,
 	},
 	"williamboman/mason-lspconfig.nvim",
-	"nvim-telescope/telescope.nvim",
-	"nvim-telescope/telescope-project.nvim",
+	{
+		"nvim-telescope/telescope.nvim",
+
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = { "node_modules", ".git/" },
+					layout_strategy = "vertical",
+					path_display = { truncate = 4 },
+				},
+				pickers = { find_files = { hidden = true } },
+				extensions = {
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
+				},
+			})
+		end,
+	},
+	"nvim-treesitter/nvim-treesitter",
 	"neovim/nvim-lspconfig",
-	"tami5/lspsaga.nvim",
+
 	"hrsh7th/cmp-nvim-lsp",
 	"hrsh7th/cmp-buffer",
 	"hrsh7th/cmp-path",
 	"hrsh7th/cmp-cmdline",
 	"hrsh7th/nvim-cmp",
-	"hrsh7th/vim-vsnip",
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+
 	"onsails/lspkind-nvim",
-	"nvim-treesitter/nvim-treesitter",
 	"dense-analysis/ale",
-	"preservim/nerdtree",
-	"dstein64/vim-startuptime",
 	"tpope/vim-surround",
-	"shatur/neovim-ayu",
-	"nvim-lualine/lualine.nvim",
-	"kyazdani42/nvim-web-devicons",
-	"sotte/presenting.vim",
-	"ryanoasis/vim-devicons",
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { { "kyazdani42/nvim-web-devicons", name = "kyazdani42-webdevicons" } },
+		config = function()
+			require("lualine").setup()
+		end,
+	},
+	{
+		"glepnir/lspsaga.nvim",
+        event = "BufRead",
+        dependencies = { { "nvim-tree/nvim-web-devicons" } },
+		config = function()
+			require("lspsaga").setup({})
+		end,
+	},
 	"ggandor/leap.nvim",
+})
+
+-- TODO: move this out of here :)
+local lspkind = require("lspkind")
+local cmp = require("cmp")
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" }, -- For luasnip users.
+	}, {
+		{ name = "buffer" },
+	}),
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol", -- show only symbol annotations
+			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+		}),
+	},
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype("gitcommit", {
+	sources = cmp.config.sources({
+		{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+	}, {
+		{ name = "buffer" },
+	}),
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
+})
+
+-- Set up lspconfig.
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("lspconfig")["sumneko_lua"].setup({
+	capabilities = capabilities,
+})
+
+require("lspconfig")["tsserver"].setup({
+	capabilities = capabilities,
+})
+
+require("lspconfig")["cssls"].setup({
+	capabilities = capabilities,
+})
+
+require("lspconfig")["emmet_ls"].setup({
+	capabilities = capabilities,
+})
+
+require("lspconfig")["pyright"].setup({
+	capabilities = capabilities,
 })
