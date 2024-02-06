ce674686989c60de58a7fc27418e055d045dee05
Guilherme Oliveira <htoguilherme@gmail.com>
2024-02-06 23:00:19 +1300

    feat(nixos): change hostname

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "gui"; # Define your hostname.
   networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
