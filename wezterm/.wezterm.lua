c9f7ef93cc32d7c55aabe7bf18d7635901fe7ebe
Guilherme Oliveira <goliveira@atlassian.com>
2025-06-04 09:44:48 +1200

    feat(wezterm): disable audio bell

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Gruvbox dark, soft (base16)'
+config.color_scheme = "Gruvbox dark, soft (base16)"
 
 -- Font config
 --
@@ -26,6 +26,8 @@ config.cell_width = 0.9
 
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
+
+config.audible_bell = "Disabled"
 
 -- and finally, return the configuration to wezterm
 return config
