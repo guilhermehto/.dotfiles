bb120d686af53c19d202383fef8e5b4b9d4ecc17
Guilherme Oliveira <htoguilherme@gmail.com>
2025-12-02 23:20:46 +1300

    feat(nvim): format on save

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,5 +1,20 @@
+-- Format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
 -- tags: formatting, auto format, autoformat, auto-formatting
 return {
 	"stevearc/conform.nvim",
-	opts = {},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "biome", "prettier" },
+			typescript = { "biome", "prettier" },
+			javascriptreact = { "biome", "prettier" },
+			typescriptreact = { "biome", "prettier" },
+		},
+	},
 }
