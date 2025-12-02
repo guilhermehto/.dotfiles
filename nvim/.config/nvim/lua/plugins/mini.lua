d04d33d6b6047552abc54a18b0c37bc5044f2f7c
Guilherme Oliveira <htoguilherme@gmail.com>
2025-12-02 23:00:41 +1300

    feat(nvim): config mini.files

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,76 +1,118 @@
+-- Customize how the mini.files window looks like, set blend
+vim.api.nvim_create_autocmd('User', {
+	pattern = 'MiniFilesWindowOpen',
+	callback = function(args)
+		local win_id = args.data.win_id
+
+		-- Customize window-local settings
+		vim.wo[win_id].winblend = 10
+		local config = vim.api.nvim_win_get_config(win_id)
+		config.border, config.title_pos = 'double', 'right'
+		vim.api.nvim_win_set_config(win_id, config)
+	end,
+})
+
+-- Ensure height
+vim.api.nvim_create_autocmd('User', {
+	pattern = 'MiniFilesWindowUpdate',
+	callback = function(args)
+		local config = vim.api.nvim_win_get_config(args.data.win_id)
+
+		-- Ensure fixed height
+		config.height = 20
+
+		-- Ensure no title padding
+		local n = #config.title
+		config.title[1][1] = config.title[1][1]:gsub('^ ', '')
+		config.title[n][1] = config.title[n][1]:gsub(' $', '')
+
+		vim.api.nvim_win_set_config(args.data.win_id, config)
+	end,
+})
+
+
 return {
 	{
 		"nvim-mini/mini.files",
 		version = false,
-		opts = {},
+		opts = {
+			mappings = {
+				go_in = 'L',
+				go_in_plus = 'l',
+			}
+		},
 		keys = {
 			{
 				"<leader>fb",
 				function()
-					require("mini.files").open()
+					local MiniFiles = require("mini.files")
+					local _ = MiniFiles.close() or MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+					vim.defer_fn(function()
+						MiniFiles.reveal_cwd()
+					end, 30)
 				end,
 				desc = "[F]ile [B]rowser",
-			},
 		},
 	},
-	{ "nvim-mini/mini.notify", version = false, opts = {} },
-	{ "nvim-mini/mini.icons", version = false, opts = {} },
-	{ "nvim-mini/mini.animate", version = false, opts = {} },
-	{ "nvim-mini/mini.statusline", version = false, opts = {} },
-	-- {
+},
+{ "nvim-mini/mini.notify", version = false, opts = {} },
+{ "nvim-mini/mini.icons", version = false, opts = {} },
+{ "nvim-mini/mini.animate", version = false, opts = {} },
+{ "nvim-mini/mini.statusline", version = false, opts = {} },
+-- {
 	-- 	"nvim-mini/mini.pick",
 	-- 	version = false,
 	-- 	opts = {
-	-- 		source = {
-	-- 			files = {
-	-- 				respect_gitignore = false,
-	-- 				visibility = { hidden = true },
-	-- 			},
-	-- 		},
-	-- 	},
-	-- 	keys = {
-	-- 		{
-	-- 			"<C-p>",
-	-- 			function()
-	-- 				require("mini.pick").builtin.files({visibility = {hidden = true}})
-	-- 			end,
-	-- 		},
-	-- 		{
-	-- 			"<leader><space>",
-	-- 			function()
-	-- 				require("mini.pick").builtin.buffers()
-	-- 			end,
-	-- 		},
-	-- 	},
-	-- },
-	-- {
-	-- 	"nvim-mini/mini.extra",
-	-- 	version = false,
-	-- 	opts = {},
-	-- 	keys = {
-	-- 		{
-	-- 			"<leader>ff",
-	-- 			function()
-	-- 				require("mini.extra").pickers.buf_lines()
-	-- 			end,
-	-- 			desc = "[F]ile [F]ind",
-	-- 		},
-	-- 		{
-	-- 			"<leader>cd",
-	-- 			function()
-	-- 				require("mini.extra").pickers.diagnostic()
-	-- 			end,
-	-- 			desc = "[C]ode [D]iagnostic",
-	-- 		},
-	-- 	},
-	-- },
-	{
-		"nvim-mini/mini.hues",
-		version = false,
-		config = function()
-			require("mini.hues").setup({ background = "#002734", foreground = "#c0c8cc", n_hues = 6 })
-			vim.cmd.colorscheme("minisummer")
-		end,
-	},
-	{ "nvim-mini/mini.indentscope", version = false, opts = {} },
-}
+		-- 		source = {
+			-- 			files = {
+				-- 				respect_gitignore = false,
+				-- 				visibility = { hidden = true },
+				-- 			},
+				-- 		},
+				-- 	},
+				-- 	keys = {
+					-- 		{
+						-- 			"<C-p>",
+						-- 			function()
+							-- 				require("mini.pick").builtin.files({visibility = {hidden = true}})
+							-- 			end,
+							-- 		},
+							-- 		{
+								-- 			"<leader><space>",
+								-- 			function()
+									-- 				require("mini.pick").builtin.buffers()
+									-- 			end,
+									-- 		},
+									-- 	},
+									-- },
+									-- {
+										-- 	"nvim-mini/mini.extra",
+										-- 	version = false,
+										-- 	opts = {},
+										-- 	keys = {
+											-- 		{
+												-- 			"<leader>ff",
+												-- 			function()
+													-- 				require("mini.extra").pickers.buf_lines()
+													-- 			end,
+													-- 			desc = "[F]ile [F]ind",
+													-- 		},
+													-- 		{
+														-- 			"<leader>cd",
+														-- 			function()
+															-- 				require("mini.extra").pickers.diagnostic()
+															-- 			end,
+															-- 			desc = "[C]ode [D]iagnostic",
+															-- 		},
+															-- 	},
+															-- },
+															{
+																"nvim-mini/mini.hues",
+																version = false,
+																config = function()
+																	require("mini.hues").setup({ background = "#002734", foreground = "#c0c8cc", n_hues = 6 })
+																	vim.cmd.colorscheme("minisummer")
+																end,
+															},
+															{ "nvim-mini/mini.indentscope", version = false, opts = {} },
+														}
