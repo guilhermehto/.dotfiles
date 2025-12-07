f5ec536704f63c40e8bc7f81f581d9837e9a2651
Guilherme Oliveira <goliveira@atlassian.com>
2025-12-08 11:19:27 +1300

    chore: reformat

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,20 +1,20 @@
 -- Customize how the mini.files window looks like, set blend
-vim.api.nvim_create_autocmd('User', {
-	pattern = 'MiniFilesWindowOpen',
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesWindowOpen",
 	callback = function(args)
 		local win_id = args.data.win_id
 
 		-- Customize window-local settings
 		vim.wo[win_id].winblend = 10
 		local config = vim.api.nvim_win_get_config(win_id)
-		config.border, config.title_pos = 'double', 'right'
+		config.border, config.title_pos = "double", "right"
 		vim.api.nvim_win_set_config(win_id, config)
 	end,
 })
 
 -- Ensure height
-vim.api.nvim_create_autocmd('User', {
-	pattern = 'MiniFilesWindowUpdate',
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesWindowUpdate",
 	callback = function(args)
 		local config = vim.api.nvim_win_get_config(args.data.win_id)
 
@@ -23,13 +23,12 @@ vim.api.nvim_create_autocmd('User', {
 
 		-- Ensure no title padding
 		local n = #config.title
-		config.title[1][1] = config.title[1][1]:gsub('^ ', '')
-		config.title[n][1] = config.title[n][1]:gsub(' $', '')
+		config.title[1][1] = config.title[1][1]:gsub("^ ", "")
+		config.title[n][1] = config.title[n][1]:gsub(" $", "")
 
 		vim.api.nvim_win_set_config(args.data.win_id, config)
 	end,
 })
-
 
 return {
 	{
@@ -37,9 +36,9 @@ return {
 		version = false,
 		opts = {
 			mappings = {
-				go_in = 'L',
-				go_in_plus = 'l',
-			}
+				go_in = "L",
+				go_in_plus = "l",
+			},
 		},
 		keys = {
 			{
@@ -55,37 +54,39 @@ return {
 			},
 		},
 	},
-	{ "nvim-mini/mini.notify", version = false, opts = {
-	} },
+	{ "nvim-mini/mini.notify", version = false, opts = {} },
 	{ "nvim-mini/mini.icons", version = false, opts = {} },
-	{ "nvim-mini/mini.animate", version = false, config = function ()
-		local animate = require('mini.animate')
-		animate.setup({
-			cursor = {
-				timing = animate.gen_timing.cubic({ duration = 200, easing = 'in', unit = 'total' })
-			},
-			scroll = {
-				timing = animate.gen_timing.cubic({ duration = 200, easing = 'in', unit = 'total' })
-			},
-			resize = {
-				timing = animate.gen_timing.cubic({ duration = 100, easing = 'in', unit = 'total' })
-			},
-			open = {
-				timing = animate.gen_timing.cubic({ duration = 200, easing = 'in', unit = 'total' })
-			},
-			close = {
-				timing = animate.gen_timing.cubic({ duration = 200, easing = 'in', unit = 'total' })
-			},
-		})
-	end
- },
+	{
+		"nvim-mini/mini.animate",
+		version = false,
+		config = function()
+			local animate = require("mini.animate")
+			animate.setup({
+				cursor = {
+					timing = animate.gen_timing.cubic({ duration = 200, easing = "in", unit = "total" }),
+				},
+				scroll = {
+					timing = animate.gen_timing.cubic({ duration = 200, easing = "in", unit = "total" }),
+				},
+				resize = {
+					timing = animate.gen_timing.cubic({ duration = 100, easing = "in", unit = "total" }),
+				},
+				open = {
+					timing = animate.gen_timing.cubic({ duration = 200, easing = "in", unit = "total" }),
+				},
+				close = {
+					timing = animate.gen_timing.cubic({ duration = 200, easing = "in", unit = "total" }),
+				},
+			})
+		end,
+	},
 	{ "nvim-mini/mini.statusline", version = false, opts = {} },
 	{
 		"nvim-mini/mini.hues",
 		version = false,
 		config = function()
 			require("mini.hues").setup({ background = "#002734", foreground = "#c0c8cc", n_hues = 6 })
-			vim.cmd.colorscheme("minisummer")
+			vim.cmd.colorscheme("miniwinter")
 		end,
 	},
 	{ "nvim-mini/mini.indentscope", version = false, opts = {} },
