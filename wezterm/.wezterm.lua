dfd2e334ffe93e7990ed2adfd4793ac3a40f2232
Guilherme Oliveira <goliveira@atlassian.com>
2026-03-31 16:27:41 -0300

    feat: change wezterm theme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Gruvbox dark, soft (base16)"
+config.color_scheme = "Ayu Dark (Gogh)"
 
 -- Font config
 --
