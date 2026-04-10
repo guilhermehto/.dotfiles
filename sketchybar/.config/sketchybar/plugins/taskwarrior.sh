9f6eaeee35ec2a6c457347a3c96f15c5030cbece
Guilherme Oliveira <htoguilherme@gmail.com>
2026-04-10 15:19:15 -0300

    feat(sketchybar): update colorscheme

--- sketchybar/.config/sketchybar/plugins/taskwarrior.sh
+++ sketchybar/.config/sketchybar/plugins/taskwarrior.sh
@@ -24,11 +24,11 @@ if [ "$TASK_COUNT" -gt 0 ] 2>/dev/null; then
   fi
 
   ICON="󰄬"
-  ICON_COLOR="0xffb8bb26"
+  ICON_COLOR="0xffaad94c"
 else
   LABEL=""
   ICON="󰄱"
-  ICON_COLOR="0xff928374"
+  ICON_COLOR="0xff686868"
 fi
 
 sketchybar --set $NAME label="$LABEL" icon="$ICON" icon.color="$ICON_COLOR"
