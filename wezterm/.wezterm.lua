f41c4a8db78620b7d97852bcf64f179a9890abd7
Guilherme Oliveira <htoguilherme@gmail.com>
2026-04-10 15:09:57 -0300

    feat(wezterm): update theme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Ayu Dark (Gogh)"
+config.color_scheme = "ayu"
 
 -- Font config
 --
