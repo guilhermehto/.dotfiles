ab9878723c715d65721d25a866be3f6b22010993
Guilherme Oliveira <htoguilherme@gmail.com>
2026-05-10 14:16:08 +1200

    feat(nvim): replace mini.files with neo-tree

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,61 +1,17 @@
--- Customize how the mini.files window looks like, set blend
-vim.api.nvim_create_autocmd("User", {
-	pattern = "MiniFilesWindowOpen",
-	callback = function(args)
-		local win_id = args.data.win_id
-
-		-- Customize window-local settings
-		vim.wo[win_id].winblend = 10
-		local config = vim.api.nvim_win_get_config(win_id)
-		config.border, config.title_pos = "double", "right"
-		vim.api.nvim_win_set_config(win_id, config)
-	end,
-})
-
--- Ensure height
-vim.api.nvim_create_autocmd("User", {
-	pattern = "MiniFilesWindowUpdate",
-	callback = function(args)
-		local config = vim.api.nvim_win_get_config(args.data.win_id)
-
-		-- Ensure fixed height
-		config.height = 20
-
-		-- Ensure no title padding
-		local n = #config.title
-		config.title[1][1] = config.title[1][1]:gsub("^ ", "")
-		config.title[n][1] = config.title[n][1]:gsub(" $", "")
-
-		vim.api.nvim_win_set_config(args.data.win_id, config)
-	end,
-})
-
 return {
-	{
-		"nvim-mini/mini.files",
-		version = false,
-		opts = {
-			mappings = {
-				go_in = "L",
-				go_in_plus = "l",
-			},
-		},
-		keys = {
-			{
-				"<leader>fb",
-				function()
-					local MiniFiles = require("mini.files")
-					local _ = MiniFiles.close() or MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
-					vim.defer_fn(function()
-						MiniFiles.reveal_cwd()
-					end, 30)
-				end,
-				desc = "[F]ile [B]rowser",
-			},
-		},
-	},
 	{ "nvim-mini/mini.notify", version = false, opts = {} },
-	{ "nvim-mini/mini.icons", version = false, opts = {} },
+	{
+		"nvim-mini/mini.icons",
+		version = false,
+		lazy = false,
+		priority = 900,
+		config = function()
+			require("mini.icons").setup()
+			-- Make plugins that look for `nvim-web-devicons` (e.g. neo-tree)
+			-- pick up mini.icons instead.
+			MiniIcons.mock_nvim_web_devicons()
+		end,
+	},
 	{
 		"nvim-mini/mini.animate",
 		version = false,
---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,61 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-mini/mini.icons",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle File Tree" },
+		{ "<leader>fb", "<cmd>Neotree toggle reveal<cr>", desc = "[F]ile [B]rowser" },
+		{
+			"<leader>fG",
+			function()
+				vim.cmd("NeotreeGitToggle")
+			end,
+			desc = "[F]ile tree [G]it status toggle",
+		},
+	},
+	opts = {
+		sources = { "filesystem", "buffers", "git_status" },
+		-- Off by default; can be slow on large repos. Toggle with :NeotreeGitToggle.
+		enable_git_status = false,
+		git_status_async = true,
+		window = {
+			position = "left",
+			width = 35,
+		},
+		filesystem = {
+			follow_current_file = { enabled = true },
+			use_libuv_file_watcher = true,
+			filtered_items = {
+				visible = true,
+				hide_dotfiles = false,
+				hide_gitignored = false,
+			},
+		},
+	},
+	config = function(_, opts)
+		require("neo-tree").setup(opts)
+
+		-- Use mini.icons' azure for folder icons instead of the muted
+		-- `Directory` highlight neo-tree links to by default.
+		vim.api.nvim_set_hl(0, "NeoTreeDirectoryIcon", { link = "MiniIconsAzure" })
+		vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", { link = "MiniIconsAzure" })
+
+		-- Per-session toggle for git decorations in the filesystem tree.
+		-- Useful on large repos where git is slow: keep it off by default
+		-- and flip on when you actually want git status visible.
+		vim.api.nvim_create_user_command("NeotreeGitToggle", function()
+			opts.enable_git_status = not opts.enable_git_status
+			require("neo-tree").setup(opts)
+			vim.cmd("Neotree refresh")
+			vim.notify(
+				"neo-tree git_status: " .. (opts.enable_git_status and "ON" or "OFF"),
+				vim.log.levels.INFO
+			)
+		end, { desc = "Toggle neo-tree git status decorations" })
+	end,
+}
