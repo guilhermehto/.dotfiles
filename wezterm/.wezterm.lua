629119f7f7d30fbf409a4a941e924f4f0b2e6ab0
Guilherme Oliveira <goliveira@atlassian.com>
2026-08-11 22:15:17 +1200

    feat(wezterm): use tokyonight storm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "ayu"
+config.color_scheme = "Tokyo Night Storm"
 
 -- Font config
 --
